03fdd2ab53db318a055e889b95bad08c585903b2
balaka <balaka.dutta99@gmail.com>
2020-06-27 19:06:59 +0200

    nothing special

--- src/app/image-component/image-component.component.ts
+++ src/app/image-component/image-component.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { FormGroup, FormControl , Validators} from '@angular/forms';
-import {MatDialog} from '@angular/material/dialog';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { map } from 'rxjs/operators';
+import { database } from 'firebase';
 
 @Component({
   selector: 'app-image-component',
@@ -13,25 +14,26 @@ import { map } from 'rxjs/operators';
 export class ImageComponentComponent implements OnInit {
   srcResult;
   frame: string;
-  imgSrc: string ='./assets/featured.jpeg';
-  selectedImage :any = null;
+  imgSrc: string = './assets/featured.jpeg';
+  selectedImage: any = null;
   isSubmitted: boolean = false;
   titleFormControl = new FormControl('', [Validators.required]);
   descriptionFormControl = new FormControl('', [Validators.required]);
   checked = false;
   indeterminate = false;
-  selected='';
+  selected = '';
   labelPosition: 'before' | 'after' = 'after';
   disabled = false;
 
-  constructor(private storage: AngularFireStorage,
-    public dialog: MatDialog, private database: AngularFirestore) { }
-    imageform: FormGroup = new FormGroup({
-      title: this.titleFormControl,
-      description: this.descriptionFormControl,
-    });
 
-    email = new FormControl('', [Validators.required, Validators.email]);
+  constructor(private storage: AngularFireStorage,
+    public dialog: MatDialog) { }
+  imageform: FormGroup = new FormGroup({
+    title: this.titleFormControl,
+    description: this.descriptionFormControl,
+  });
+
+  email = new FormControl('', [Validators.required, Validators.email]);
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
@@ -41,41 +43,36 @@ export class ImageComponentComponent implements OnInit {
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
   ngOnInit(): void {
-   this.getimageTypes();
+    //  this.getimageTypes();
   }
-  getimageTypes(){
-    var imageType;
-    var imageTypeDoc = this.database.collection('admin').doc('galleryTypes');
-    imageTypeDoc.get().subscribe(imageTypes=>console.log(imageTypes.data()));
-    
-  }
+  
   getRequiredErrorMessage(field) {
     return this.imageform.get(field).hasError('required') ? 'You must enter a value' : '';
   }
-  uploadImage(){
+  uploadImage() {
 
   }
-  chooseView(){
-    
+  chooseView() {
+
   }
   toBase64 = file => new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => resolve(reader.result);
     reader.onerror = error => reject(error);
-});
+  });
 
   async onFileSelected() {
-   const file = (<HTMLInputElement>document.getElementById('file')).files[0];
-   this.toBase64(file).then((res) => {
+    const file = (<HTMLInputElement>document.getElementById('file')).files[0];
+    this.toBase64(file).then((res) => {
       this.srcResult = res;
       const filePath = `${new Date().getTime()}`;
-      const fileRef =  this.storage.ref(filePath);
-      fileRef.putString(this.srcResult, 'data_url').then(function(snapshot) {
+      const fileRef = this.storage.ref(filePath);
+      fileRef.putString(this.srcResult, 'data_url').then(function (snapshot) {
         console.log(snapshot)
       })
       this.isSubmitted = true;
-   })
+    })
   }
   // writeNewImage(uid, username, picture, title, body) {
   //   // A post entry.
@@ -87,15 +84,15 @@ export class ImageComponentComponent implements OnInit {
   //     starCount: 0,
   //     authorPic: picture
   //   };
-  
+
   //   // Get a key for a new Post.
   //   var newPostKey = firebase.database().ref().child('posts').push().key;
-  
+
   //   // Write the new post's data simultaneously in the posts list and the user's post list.
   //   var updates = {};
   //   updates['/posts/' + newPostKey] = postData;
   //   updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-  
+
   //   return firebase.database().ref().update(updates);
   // }
 }
