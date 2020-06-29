4e2ecfe629ccea39c028cd1c47c0d4801155e079
balaka <balaka.dutta99@gmail.com>
2020-06-29 15:27:00 +0200

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -77,11 +77,7 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
     MatRadioModule,
     MatSnackBarModule,
     MatProgressBarModule,
-<<<<<<< HEAD
     RatingModule ,
-=======
-    RatingModule,
->>>>>>> d3cd2b362fcf5e17f45e4dda4779363e39a1d794
   ],
   providers: [
     {
