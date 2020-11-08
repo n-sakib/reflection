4822b4c1303b78257aedeea0ff1b901ced31b9ef
Nazmus Sakib <ns.sanim@gmail.com>
2020-11-09 00:41:41 +0100

    Added more updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,47 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent }  from './home/home.component';
 import { ArtistAuthenticationComponent }  from './artist/artist-authentication/artist-authentication.component';
 import { ArtistDashboardComponent } from './artist/artist-dashboard/artist-dashboard.component';
+import { OverviewComponent } from './profile/overview/overview.component';
+import { AuthComponent } from './profile/auth/auth.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { CartComponent } from './profile/cart/cart.component';
+import { OrderSequenceComponent } from './profile/order-sequence/order-sequence.component';
+import { ExhibitionComponent } from './exhibition/exhibition.component';
+import { PaintingsListComponent } from './paintings/paintings-list/paintings-list.component';
+import { PaintingsDetailsComponent } from './paintings/paintings-details/paintings-details.component';
+import { ArtistsListComponent } from './artists/artists-list/artists-list.component';
+import { ArtistsDetailsComponent } from './artists/artists-details/artists-details.component';
+import { PhotoToArtComponent } from './photomaniputation/photo-to-art/photo-to-art.component';
+import { ArtReproductionComponent } from './photomaniputation/art-reproduction/art-reproduction.component';
+import { FaqComponent } from './compliance/faq/faq.component';
+import { PrivacyPolicyComponent } from './compliance/privacy-policy/privacy-policy.component';
+import { TosComponent } from './compliance/tos/tos.component';
+import { SiteMapComponent } from './compliance/site-map/site-map.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'artist/login', component: ArtistAuthenticationComponent },
   { path: 'artist/dashboard', component: ArtistDashboardComponent },
+  { path: 'user/login', component: OverviewComponent },
+  { path: 'user/dashboard', component: AuthComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'reviews', component: ReviewsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'order', component: OrderSequenceComponent },
+  { path: 'exhibitions', component: ExhibitionComponent },
+  { path: 'paintings', component: PaintingsListComponent },
+  { path: 'paintings/:id', component: PaintingsDetailsComponent },
+  { path: 'artists', component: ArtistsListComponent },
+  { path: 'artists/:id', component: ArtistsDetailsComponent },
+  { path: 'photo-to-art', component: PhotoToArtComponent },
+  { path: 'art-reproduction', component: ArtReproductionComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'privacy', component: PrivacyPolicyComponent },
+  { path: 'tos', component: TosComponent },
+  { path: 'sitemap', component: SiteMapComponent }
 ];
 
 @NgModule({
