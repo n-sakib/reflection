5393712bb3c4d1321ec4d378a709840adf2b38f0
Balaka Dutta <balakadutta@Balakas-MacBook-Pro.local>
2020-12-12 17:41:05 +0100

    artwork

--- src/app/artist/artist-dashboard/artist-dashboard.component.ts
+++ src/app/artist/artist-dashboard/artist-dashboard.component.ts
@@ -26,6 +26,18 @@ export class ArtistDashboardComponent implements OnInit {
   selectedHeight = '';
   selectedWidth = '';
   selectedConcept = '';
+  selectedYear = '';
+  selectedReplica = '';
+  selectedSellOption = '';
+  selectedArtSchool = '';
+  selectedArtCertificate = '';
+  selectedArtYear = '';
+  selectedGradSchool = '';
+  selectedGradCertificate = '';
+  selectedGradYear = '';
+  selectedPGSchool ='';
+  selectedPGDegree = '';
+  selectedPGYear = '';
   public mediaLists: any[] = [{ value: 'Dual' }, { value: 'Single'}];
 
   constructor(private toastr: ToastrService, private storage: AngularFireStorage, private database: AngularFireDatabase,private afAuth: AngularFireAuth, public auth: AuthService) { }
@@ -37,7 +49,9 @@ export class ArtistDashboardComponent implements OnInit {
         console.log(this.user.displayName)
         console.log(this.user.photoURL)
     });
-  
+    
+
+    
   }
 
   toBase64 = file => new Promise((resolve, reject) => {
@@ -47,28 +61,41 @@ export class ArtistDashboardComponent implements OnInit {
     reader.onerror = error => reject(error);
   });
 
-  async onImageSelected() {
-    
-    const file = (<HTMLInputElement>document.getElementById('imageFile')).files[0];
-    this.toBase64(file).then(() => {
-      const filePath = `${new Date().getTime()}`;
-      const fileRef = this.storage.ref(filePath);
-      const task = this.storage.upload(filePath, file);
-      task
-        .snapshotChanges()
-        .pipe(
-          finalize(() => {
-            var downloadURL = fileRef.getDownloadURL();
-            downloadURL.subscribe(url => {
-              this.isImageSubmitted = true;
-              this.selectedImageURL = url;
-            
-            })
-          })
-        )
-        .subscribe();
-    })
+  onImageSelected(event) {
+    this.isImageSubmitting = true;
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]); 
+      reader.onload = (event: any) => {
+        this.selectedImageURL = event.target.result;
+        // console.log(this.selectedImageURL)
+        // this.isImageSubmitted = true
+      }
+    }
   }
+
+      
+    
+    // const file = (<HTMLInputElement>document.getElementById('imageFile')).files[0];
+    // this.toBase64(file).then(() => {
+    //   const filePath = `${new Date().getTime()}`;
+    //   const fileRef = this.storage.ref(filePath);
+    //   const task = this.storage.upload(filePath, file);
+    //   task
+    //     .snapshotChanges()
+    //     .pipe(
+    //       finalize(() => {
+    //         var downloadURL = fileRef.getDownloadURL();
+    //         downloadURL.subscribe(url => {
+    //           this.isImageSubmitted = true;
+    //           this.selectedImageURL = url;
+            
+    //         })
+    //       })
+    //     )
+    //     .subscribe();
+    // })
+  
 
   addTitle($event) {
     var value = $event.target.value;
@@ -95,6 +122,57 @@ export class ArtistDashboardComponent implements OnInit {
     var value = $event.target.value;
     this.selectedConcept = value;
   }
+  addYear($event){
+    var value = $event.target.value;
+    this.selectedYear = value;
+  }
+  addSellOption(sell){
+    // var value = $event.target.value;
+    this.selectedSellOption = sell;
+    console.log(this.selectedSellOption)
+  }
+  addReplicaOrder(rep){
+    // var value = $event.target.value;
+    this.selectedReplica= rep;
+    console.log(this.selectedReplica)
+  }
+  addArtSchool($event){
+    var value = $event.target.value;
+    this.selectedArtSchool = value;
+  }
+  addArtCertificate($event){
+    var value = $event.target.value;
+    this.selectedArtCertificate = value;
+  }
+  addArtYear($event){
+    var value = $event.target.value;
+    this.selectedArtYear = value;
+  } 
+  addGradSchool($event){
+    var value = $event.target.value;
+    this.selectedGradSchool = value;
+  }
+  addGradCertificate($event){
+    var value = $event.target.value;
+    this.selectedGradCertificate= value;
+  }
+  addGradYear($event){
+    var value = $event.target.value;
+    this.selectedGradYear = value;
+  }
+  addPGSchool($event){
+    var value = $event.target.value;
+    this.selectedPGSchool = value;
+  }
+  addPGDegree($event){
+    var value = $event.target.value;
+    this.selectedPGDegree = value;
+  }
+  addPGYear($event){
+    var value = $event.target.value;
+    this.selectedPGYear = value;
+  }
+
 
   publish() {
     // A post entry.
@@ -106,20 +184,55 @@ export class ArtistDashboardComponent implements OnInit {
       concept:  this.selectedConcept,
       height:  this.selectedHeight,
       width: this.selectedWidth ,
+      sellOption: this.selectedSellOption,
+      replicaOrder: this.selectedReplica
     };
 
     console.log(postData)
     // Get a key for a new Post.
-    this.database.list(`images/`).push(postData).then(() => {
+    this.database.list(`artWork/`).push(postData).then(() => {
         this.selectedType = '',
         this.selectedImageURL = '',
         this.selectedTitle = '',
         this.selectedCategory='',
         this.selectedConcept= '',
         this.selectedHeight= '',
-        this.selectedWidth  = ''
+        this.selectedWidth  = '',
+        this.selectedSellOption = '',
+        this.selectedReplica = ''
     })
     this.toastr.success('Post is Done!');
   }
 
+  publishEducation() {
+    // A post entry.
+    var postData = {
+      artSchool: this.selectedArtSchool,
+      artDegree: this.selectedArtCertificate,
+      artYear: this.selectedArtYear,
+      gradSchool: this.selectedGradSchool ,
+      gradDegree:  this.selectedGradCertificate,
+      gradYear:  this.selectedGradYear,
+      pgSchool: this.selectedPGSchool,
+      pgDegree: this.selectedPGDegree,
+      pgYear: this.selectedPGYear
+    };
+
+    console.log(postData)
+    // Get a key for a new Post.
+    this.database.list(`artist_education/`).push(postData).then(() => {
+      this.selectedArtSchool= '',
+      this.selectedArtCertificate = '',
+      this.selectedArtYear = '',
+      this.selectedGradSchool ='',
+      this.selectedGradCertificate = '',
+      this.selectedGradYear= '',
+      this.selectedPGSchool = '',
+      this.selectedPGDegree = '',
+      this.selectedPGYear = ''
+    })
+    this.toastr.success('Post is Done!');
+  }
+
+  
 }
